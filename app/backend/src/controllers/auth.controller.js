4d1562cdcf1cae799dabbdcfb28ab7c76606078a
essadeq billouche <essadeq.billouche@gmail.com>
2026-04-23 16:16:09 +0100

    feat: FOC-15 implement login functionality with token generation

--- app/backend/src/controllers/auth.controller.js
+++ app/backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import { creatUserService } from "../services/auth.service.js";
+import { creatUserService, loginService } from "../services/auth.service.js";
 import { generateToken } from "../utils/jwt.js";
 
 export const register = async (req, res) => {
@@ -22,6 +22,37 @@ export const register = async (req, res) => {
         return res.status(201).json({
             status: "success",
             token, // <-- send the token back
+            data: { user }
+        });
+    } catch (error) {
+        res.status(error.statusCode || 500).json({
+            status: error.status || "error",
+            message: error.message
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                status: "error",
+                message: "Please provide email and password"
+            });
+        }
+
+        const user = await loginService(email, password);
+
+        // Generate the token
+        const token = generateToken({ userId: user._id, role: user.role });
+
+        user.password = undefined;
+
+        return res.status(200).json({
+            status: "success",
+            token,
             data: { user }
         });
     } catch (error) {

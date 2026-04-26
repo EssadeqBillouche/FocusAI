1f4746f091aff5a5ca790f88b149e9ab72648a1e
Essadeq billouche <essadeq.billouche@gmail.com>
2026-04-26 17:13:31 +0000

    feat: ajouter la route pour obtenir les insights du tableau de bord

--- app/backend/src/presentation/dashboard.routes.js
+++ app/backend/src/presentation/dashboard.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getDashboardStats } from '../controllers/dashboard.controller.js';
+import { getDashboardInsights, getDashboardStats } from '../controllers/dashboard.controller.js';
 import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
+router.get('/insights', protectRoute, getDashboardInsights);
 router.get('/stats', protectRoute, getDashboardStats);
 
 export default router;

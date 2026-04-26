8efeb3d7add22ff41252669772d01267b37c4f13
Essadeq billouche <essadeq.billouche@gmail.com>
2026-04-26 17:13:23 +0000

    feat: ajouter les fonctions pour obtenir les tâches prioritaires et les recommandations de tâches

--- app/backend/src/controllers/task.controller.js
+++ app/backend/src/controllers/task.controller.js
@@ -2,7 +2,9 @@ import {
 	createTaskService,
 	deleteTaskService,
 	getTaskByIdService,
+	getTaskRecommendationsService,
 	getTasksService,
+	getSmartPriorityTasksService,
 	updateTaskService,
 } from '../services/task.service.js';
 
@@ -66,6 +68,34 @@ export const deleteTask = async (req, res, next) => {
 		return res.status(204).json({
 			status: 'success',
 			data: null,
+		});
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getSmartPriorityTasks = async (req, res, next) => {
+	try {
+		const tasks = await getSmartPriorityTasksService(req.user._id, req.query);
+
+		return res.status(200).json({
+			status: 'success',
+			results: tasks.length,
+			data: { tasks },
+		});
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getTaskRecommendations = async (req, res, next) => {
+	try {
+		const recommendations = await getTaskRecommendationsService(req.user._id, req.query.limit);
+
+		return res.status(200).json({
+			status: 'success',
+			results: recommendations.length,
+			data: { recommendations },
 		});
 	} catch (error) {
 		next(error);

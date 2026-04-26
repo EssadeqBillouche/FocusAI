da2322f1ff9e2b1715959acdfbd4b9fadb1c7c92
Essadeq billouche <essadeq.billouche@gmail.com>
2026-04-26 16:00:22 +0000

    feat: ajouter les routes de tâches et de tableau de bord à l'application

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -1,15 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import dbConnect from './config/db.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 import authRoutes from "./routes/auth.routes.js"
+import taskRoutes from './presentation/task.routes.js';
+import dashboardRoutes from './presentation/dashboard.routes.js';
 
 dotenv.config(); 
 
 const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
+
 dbConnect();
-app.use(errorHandler);
+
 app.use("/api-v1/auth", authRoutes)
+app.use('/api-v1/tasks', taskRoutes);
+app.use('/api-v1/dashboard', dashboardRoutes);
+
+app.use(errorHandler);
 
 export default app;

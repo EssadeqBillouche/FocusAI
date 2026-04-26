a1acdcec61951b016cfd4afd708ba9e73fd58e42
Essadeq billouche <essadeq.billouche@gmail.com>
2026-04-26 16:01:29 +0000

    feat: ajouter la validation et la vérification des requêtes pour l'enregistrement et la connexion

--- app/backend/src/routes/auth.routes.js
+++ app/backend/src/routes/auth.routes.js
@@ -1,11 +1,16 @@
 import express from "express"
 import { register, login } from "../controllers/auth.controller.js"
+import {
+	loginValidation,
+	registerValidation,
+	validateRequest,
+} from "../middlewares/validate.middleware.js";
 
 const router = express.Router();
 
 
-router.post('/register',register)
-router.post('/login', login);
+router.post('/register', registerValidation, validateRequest, register)
+router.post('/login', loginValidation, validateRequest, login);
 
 
 export default router;
